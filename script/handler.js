43f698fce8826c0c29c78d9e7a8b26a2ed3c5c44
Boam <boam.tchegli30@gmail.com>
2020-03-19 00:08:48 +0100

    update index.html &css

---
+++ script/handler.js
@@ -0,0 +1,6 @@
+const bands = ['The band', 'An old man', 'counterparts', 'Anywhere but here', 'The Midway state', 'The bled'];
+
+function strip(bandName){
+    return bandName.replace(a,the ,an).trim();
+    console.log();
+}
